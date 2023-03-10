aa2b8ce1bf50bf703061c6be3077e312834c86ad
Danyal Imran <dan.imran97@gmail.com>
2023-03-10 01:17:40 -0500

    initial commit with superplate

---
+++ src/App.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+
+import { Refine, GitHubBanner } from "@pankod/refine-core";
+import {
+  notificationProvider,
+  RefineSnackbarProvider,
+  CssBaseline,
+  GlobalStyles,
+  Layout,
+  ThemeProvider,
+  LightTheme,
+  ReadyPage,
+  ErrorComponent,
+} from "@pankod/refine-mui";
+
+import routerProvider from "@pankod/refine-react-router-v6";
+import dataProvider from "@pankod/refine-simple-rest";
+
+function App() {
+  return (
+    <>
+      <GitHubBanner />
+      <ThemeProvider theme={LightTheme}>
+        <CssBaseline />
+        <GlobalStyles styles={{ html: { WebkitFontSmoothing: "auto" } }} />
+        <RefineSnackbarProvider>
+          <Refine
+            notificationProvider={notificationProvider}
+            Layout={Layout}
+            ReadyPage={ReadyPage}
+            catchAll={<ErrorComponent />}
+            routerProvider={routerProvider}
+            dataProvider={dataProvider("https://api.fake-rest.refine.dev")}
+          />
+        </RefineSnackbarProvider>
+      </ThemeProvider>
+    </>
+  );
+}
+
+export default App;
