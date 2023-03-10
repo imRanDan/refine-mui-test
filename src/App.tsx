2ddee3c2a79aa7e5119a23577d4708662790f6bf
Danyal Imran <dan.imran97@gmail.com>
2023-03-10 01:25:38 -0500

    inital commit :p

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,47 @@
-import React from "react";
-
-import { Refine, GitHubBanner } from "@pankod/refine-core";
+import { Refine } from "@pankod/refine-core";
 import {
-  notificationProvider,
-  RefineSnackbarProvider,
-  CssBaseline,
-  GlobalStyles,
-  Layout,
-  ThemeProvider,
-  LightTheme,
-  ReadyPage,
-  ErrorComponent,
+    Layout,
+    ReadyPage,
+    ErrorComponent,
+    LightTheme,
+    CssBaseline,
+    GlobalStyles,
+    ThemeProvider,
+    RefineSnackbarProvider,
+    notificationProvider,
 } from "@pankod/refine-mui";
-
 import routerProvider from "@pankod/refine-react-router-v6";
 import dataProvider from "@pankod/refine-simple-rest";
+import { MuiInferencer } from "@pankod/refine-inferencer/mui";
 
-function App() {
-  return (
-    <>
-      <GitHubBanner />
-      <ThemeProvider theme={LightTheme}>
-        <CssBaseline />
-        <GlobalStyles styles={{ html: { WebkitFontSmoothing: "auto" } }} />
-        <RefineSnackbarProvider>
-          <Refine
-            notificationProvider={notificationProvider}
-            Layout={Layout}
-            ReadyPage={ReadyPage}
-            catchAll={<ErrorComponent />}
-            routerProvider={routerProvider}
-            dataProvider={dataProvider("https://api.fake-rest.refine.dev")}
-          />
-        </RefineSnackbarProvider>
-      </ThemeProvider>
-    </>
-  );
-}
+const App: React.FC = () => {
+    return (
+        <ThemeProvider theme={LightTheme}>
+            <CssBaseline />
+            <GlobalStyles styles={{ html: { WebkitFontSmoothing: "auto" } }} />
+            <RefineSnackbarProvider>
+                <Refine
+                    routerProvider={routerProvider}
+                    dataProvider={dataProvider(
+                        "https://api.fake-rest.refine.dev",
+                    )}
+                    notificationProvider={notificationProvider}
+                    Layout={Layout}
+                    ReadyPage={ReadyPage}
+                    catchAll={<ErrorComponent />}
+                    resources={[
+                        {
+                            name: "products",
+                            list: MuiInferencer,
+                            show: MuiInferencer,
+                            create: MuiInferencer,
+                            edit: MuiInferencer,
+                        },
+                    ]}
+                />
+            </RefineSnackbarProvider>
+        </ThemeProvider>
+    );
+};
 
 export default App;
